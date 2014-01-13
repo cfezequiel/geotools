e98e77a01d1c35eb8dffd69322d01ddcdc918d86
cfezequiel <cafezequiel@gmail.com>
2014-01-13 19:14:17 +0800

    Modified gistools/setup.py to version 0.3

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='gistools',
-      version='0.2',
+      version='0.3',
       description='Collection of GIS utility scripts and modules',
       url='http://TODO/this/later',
       author='Carlos Ezequiel',
