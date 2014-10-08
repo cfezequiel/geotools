1939e158027649979459dbc36c01fde2840f4c34
Carlos Ezequiel <cafezequiel@gmail.com>
2014-10-08 18:24:54 +0800

    Modified version number of gistools package

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='gistools',
-      version='0.4',
+      version='0.6',
       description='Collection of GIS utility scripts and modules',
       url='http://TODO/this/later',
       author='Carlos Ezequiel',
