6f919de4bee14cef1dc95a1adc7b559aa73a23d7
Carlos Ezequiel <cafezequiel@gmail.com>
2015-03-07 16:16:53 +0800

    Modified setup.py to include wf2grid and jsonify tools

--- setup.py
+++ setup.py
@@ -8,5 +8,6 @@ setup(name='gistools',
       author_email='carlosezequiel@skyeyeproject.com',
       license='MIT',
       packages=['gistools'],
-      scripts=['bin/georef.py', 'bin/genkml.py', 'bin/label.py', 'bin/georef_vid.py'],
+      scripts=['bin/georef.py', 'bin/genkml.py', 'bin/label.py', 'bin/georef_vid.py',
+      'bin/wf2grid.py', 'bin/jsonify.py'],
       zip_safe=False)
