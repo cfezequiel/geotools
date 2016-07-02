5d900a970f43a2b66b86336f49f5b14b6d47fb7f
cezequiel <cezequiel@datalicious.com>
2016-07-02 17:51:57 +0800

    Added image quality filter to jsonify tool

--- bin/jsonify.py
+++ bin/jsonify.py
@@ -1,10 +1,10 @@
 '''Generate JSON file from orthomosaic tile image + XML metadata.'''
 
 import sys
-from xml.etree import ElementTree as ET
-from os import listdir, path 
+from os import listdir, path
 import json
 from PIL import Image
+import numpy as np
 
 from gistools.crs import utm2dd
 from gistools.fmt import WorldFile, MMTile
@@ -16,10 +16,24 @@ BASE_URL = 'https://s3-ap-southeast-1.amazonaws.com/'
 ZONE = 51
 IS_NORTH = True
 
+
+def is_ok_image(img):
+    '''img = PIL Image.'''
+
+    arr = np.asarray(img.convert('L'))
+    size = img.size[0] * img.size[1]
+    bin_size = 2 ** img.bits
+    hist = np.histogram(arr, bins=bin_size, range=(0, bin_size - 1),
+                        density=False)[0]
+    norm_hist = hist * 1.0 / size
+    return int(norm_hist[0] < 0.10)
+
+
 if __name__ == '__main__':
 
     if len(sys.argv) < 3:
-        print 'Usage: python %s <.JPG+.JGw directory> <AWS s3 path>>' % path.basename(sys.argv[0])
+        print 'Usage: python %s <.JPG+.JGw directory> <AWS s3 path>>' % \
+            path.basename(sys.argv[0])
         print 'Current AWS URL: %s' % BASE_URL
         exit(1)
 
@@ -28,8 +42,8 @@ if __name__ == '__main__':
     src_dir = sys.argv[1]
 
     # Read input directory images
-    tile_image_files = ['%s/%s' % (src_dir, f) for f in listdir(src_dir) \
-            if path.splitext(f)[1] in VALID_FILE_EXT]
+    tile_image_files = ['%s/%s' % (src_dir, f) for f in listdir(src_dir)
+                        if path.splitext(f)[1] in VALID_FILE_EXT]
 
     # for each file
     tiles = []
@@ -37,10 +51,14 @@ if __name__ == '__main__':
         # Get worldfile and extract coordinates
         print 'Processing ', f
         # Get file size
-        im = Image.open(f)
-        image_size = list(im.size)
+        img = Image.open(f)
+        image_size = list(img.size)
         imgwidth = image_size[0]
         imgheight = image_size[1]
+
+        # Skip unacceptable images
+        if not is_ok_image(img):
+            continue
 
         world_file = path.splitext(f)[0] + '.JGw'
         wf = WorldFile(world_file, imgwidth=imgwidth, imgheight=imgheight)
@@ -54,7 +72,6 @@ if __name__ == '__main__':
 
     # Create JSON file
     with open('output.json', 'wt') as fp:
-       fp.write(json.dumps(tiles, sort_keys=True, indent=4))
+        fp.write(json.dumps(tiles, sort_keys=True, indent=4))
 
     print '%d tile_image_files processed' % len(tile_image_files)
-
--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
 setup(name='gistools',
-      version='0.7',
+      version='0.8',
       description='Collection of GIS utility scripts and modules',
       url='http://TODO/this/later',
       author='Carlos Ezequiel',
       author_email='carlosezequiel@skyeyeproject.com',
       license='MIT',
       packages=['gistools'],
-      scripts=['bin/georef.py', 'bin/genkml.py', 'bin/label.py', 'bin/georef_vid.py',
-      'bin/wf2grid.py', 'bin/jsonify.py'],
+      scripts=['bin/georef.py', 'bin/genkml.py', 'bin/label.py',
+               'bin/georef_vid.py', 'bin/wf2grid.py', 'bin/jsonify.py'],
       zip_safe=False)
